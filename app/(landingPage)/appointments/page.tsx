7df2214b6b164512169204e25ecea3ea06397a3f
OnsJannet <onsjannet@gmail.com>
2025-01-06 17:36:25 +0100

    update

--- app/(landingPage)/appointments/page.tsx
+++ app/(landingPage)/appointments/page.tsx
@@ -91,7 +91,7 @@ const Page = () => {
     const fetchAppointments = async () => {
       const token = localStorage.getItem('access');
       try {
-        const response = await fetch('https://test-roshita.net/api/appointment-reservations/', {
+        const response = await fetch('https://test-roshita.net/api/user-appointment-reservations/', {
           method: 'GET',
           headers: {
             'accept': 'application/json',
